d2b3e659003012dfb44e7f3f254067031d949aa2
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2018-12-05 13:08:28 -0800

    Update Traffic_light.s

--- hardware/Traffic_light.s
+++ hardware/Traffic_light.s
@@ -2,17 +2,17 @@
 .equ OUTPUT, 1
 .equ LOW, 0
 .equ HIGH, 1
-.equ WALK_GRN_PIN, 22	// wiringPi 22
-.equ DRIVE_RED_PIN, 23	// wiringPi 23
+.equ WALK_GRN_PIN, 22	// wiringPi 22 - green light for walk
+.equ DRIVE_RED_PIN, 23	// wiringPi 23 - red light for drive
 .equ DRIVE_GRN_PIN, 24	// wiringPi 24, also connected to red walking light
-.equ YLW_PIN, 25	// wiringPi 25
+.equ YLW_PIN, 25	// wiringPi 25 - yellow light for drive
 
 .equ PED_PASS_PIN, 29	// wiringPi 29 - pedestrian passing pin
-.equ STP_PIN, 21
+.equ STP_PIN, 28	// wiringPi 29 - program stop pin
 .equ STOP_TRAFFIC_S, 5		// pause in seconds
 
 .section .rodata
-message: .asciz "Press button to walk. Press and hold to turn off.\n"
+message: .asciz "Press the middle button to walk.\n"
 
 .text
 .global main
@@ -110,6 +110,8 @@ readWalkBotton:
 	pop {pc}
 
 ped_passing:
+	push {lr}
+	
 	mov r0,	#DRIVE_RED_PIN
 	bl pinOn
 
@@ -119,11 +121,11 @@ ped_passing:
 	mov r0, #WALK_GRN_PIN
 	bl pinOn
 
-	ldr r0, =#5000 //delay(10000); // delay for 10000 milliseconds or 10 seconds
+	ldr r0, =#5000 //delay(x); // delay for x milliseconds or x/1000 seconds
 	bl delay
 
-	//mov r1, #0
-//light_flash:
+	mov r4, #6
+light_flash:
 	mov r0, #YLW_PIN
 	bl pinOn
 
@@ -142,10 +144,9 @@ ped_passing:
 	ldr r0, =#500
 	bl delay
 
-	//add r1, #1
-	//cmp r1, #400
-	//blt light_flash
-//stop_walk:
+	subs r4, #1
+	bgt light_flash
+stop_walk:
 	mov r0,	#DRIVE_GRN_PIN
 	bl pinOn
 
@@ -154,16 +155,13 @@ ped_passing:
 
 	mov r0, #WALK_GRN_PIN
 	bl pinOff
+	
+	pop {pc}
 
-action: // r2 holds the number of seconds to delay
-	// return value: r0=0, no user interation; r0=1 user pressed stop button
+action: // r2 holds the number of seconds for drive red light
+	// return value: r0=0, nothing happened; r0=1 user pressed stop button
 	push {lr}
-
 	mov r5, r2
-
-	//mov r0, #0
-	//bl time
-	//mov r4, r0
 do_whl:
 	bl readStopButton
 	cmp r0, #HIGH
