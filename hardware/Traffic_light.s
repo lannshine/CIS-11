559cfa7a37e8c44625fbb1f38d1f6656c781ca48
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2018-12-05 12:38:02 -0800

    Update Traffic_light.s

--- hardware/Traffic_light.s
+++ hardware/Traffic_light.s
@@ -4,14 +4,15 @@
 .equ HIGH, 1
 .equ WALK_GRN_PIN, 22	// wiringPi 22
 .equ DRIVE_RED_PIN, 23	// wiringPi 23
-.equ DRIVE_GRN_PIN, 24	// wiringPi 24, also connected to WALK_RED_PIN
-.equ YLW_PIN, 25	// wiringPi 25 - only drive has YELLOW PIN
+.equ DRIVE_GRN_PIN, 24	// wiringPi 24, also connected to red walking light
+.equ YLW_PIN, 25	// wiringPi 25
 
-.equ STP_PIN, 29	// wiringPi 29 - STOP PIN
-.equ FLASH_S, 5		// pause in seconds
+.equ PED_PASS_PIN, 29	// wiringPi 29 - pedestrian passing pin
+.equ STP_PIN, 21
+.equ STOP_TRAFFIC_S, 5		// pause in seconds
 
 .section .rodata
-out_s: .asciz "%d, r4=%d, r5=%d\n"
+message: .asciz "Press button to walk. Press and hold to turn off.\n"
 
 .text
 .global main
@@ -19,6 +20,9 @@ main:
 	push {lr}
 	bl wiringPiSetup // wiringPiSetup() - initialize the wiringPi library
 
+	mov r0, #PED_PASS_PIN
+	bl setPinInput
+	
 	mov r0, #STP_PIN
 	bl setPinInput
 
@@ -34,7 +38,7 @@ main:
   	mov r0, #YLW_PIN
 	bl setPinOutput
   
-	mov r0, #DRIVE_GRN_PIN
+	mov r0, #DRIVE_GRN_PIN // default with green light for cars on
 	bl pinOn
 
 	mov r0, #WALK_GRN_PIN
@@ -46,12 +50,12 @@ main:
 	mov r0, #YLW_PIN
 	bl pinOff
 lp:
-	mov r2, #FLASH_S
+	mov r2, #STOP_TRAFFIC_S
 	bl action
 
 	cmp r0, #1
 	beq end_lp
-
+	
 	bal lp
 end_lp:
 	mov r0, #WALK_GRN_PIN
@@ -92,10 +96,16 @@ pinOff:
 	mov r1, #LOW
 	bl digitalWrite
 	pop {pc}
-
+	
 readStopButton:
 	push {lr}
 	mov r0, #STP_PIN
+	bl digitalRead
+	pop {pc}
+	
+readWalkBotton:
+	push {lr}
+	mov r0, #PED_PASS_PIN
 	bl digitalRead
 	pop {pc}
 
@@ -131,60 +141,6 @@ ped_passing:
 
 	ldr r0, =#500
 	bl delay
-//2nd	
-	mov r0, #YLW_PIN
-	bl pinOn
-
-	mov r0, #WALK_GRN_PIN
-	bl pinOn
-
-	ldr r0, =#500
-	bl delay
-
-	mov r0, #YLW_PIN
-	bl pinOff
-
-	mov r0, #WALK_GRN_PIN
-	bl pinOff
-
-	ldr r0, =#500
-	bl delay
-//3rd
-	mov r0, #YLW_PIN
-	bl pinOn
-
-	mov r0, #WALK_GRN_PIN
-	bl pinOn
-
-	ldr r0, =#500
-	bl delay
-
-	mov r0, #YLW_PIN
-	bl pinOff
-
-	mov r0, #WALK_GRN_PIN
-	bl pinOff
-
-	ldr r0, =#500
-	bl delay
-//4th
-	mov r0, #YLW_PIN
-	bl pinOn
-
-	mov r0, #WALK_GRN_PIN
-	bl pinOn
-
-	ldr r0, =#500
-	bl delay
-
-	mov r0, #YLW_PIN
-	bl pinOff
-
-	mov r0, #WALK_GRN_PIN
-	bl pinOff
-
-	ldr r0, =#500
-	bl delay
 
 	//add r1, #1
 	//cmp r1, #400
@@ -199,10 +155,9 @@ ped_passing:
 	mov r0, #WALK_GRN_PIN
 	bl pinOff
 
-action: // r0 holds pin to turn off, r1 holds pin to turn on
-	// r2 holds the number of seconds to delay
+action: // r2 holds the number of seconds to delay
 	// return value: r0=0, no user interation; r0=1 user pressed stop button
-	push {r4,r5,lr}
+	push {lr}
 
 	mov r5, r2
 
@@ -212,9 +167,13 @@ action: // r0 holds pin to turn off, r1 holds pin to turn on
 do_whl:
 	bl readStopButton
 	cmp r0, #HIGH
+	beq action_done
+	
+	bl readWalkBotton
+	cmp r0, #HIGH
 	beq ped_passing
 	
 	b do_whl
 	mov r0, #0
 action_done:
-	pop {r4,r5,pc}
+	pop {pc}
